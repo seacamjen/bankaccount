95cb92adf34bc2849dd614c084bcd8483f4785f5
Cameron Jensen and Megan Warnock <cameron@>
2017-02-28 15:14:03 -0800

    Add functionality to acount for deposit and withdrawal at the same time.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,24 @@
 //Busniness logic
-function Account (inputName, firstMoney) {
+function Account (inputName, firstMoney, deposit, withdrawal) {
   this.inputName = inputName;
   this.firstMoney = firstMoney;
+  this.deposit = deposit;
+  this.withdrawal = withdrawal;
 }
 
+Account.prototype.addDeposit = function () {
+ return total + this.deposit;
+}
 
+Account.prototype.subWithdra = function () {
+  return total - this.withdrawal;
+}
 
+Account.prototype.doubletrans = function () {
+  return total + (this.deposit - this.withdrawal);
+}
+
+var total;
 //front end logic
  $(function(){
   $("#register").submit(function(){
@@ -14,9 +27,31 @@ function Account (inputName, firstMoney) {
     var name = $("input#name").val();
     var initialDeposit = parseInt($("input#initialDeposit").val());
 
-    var money = new Account(name, initialDeposit);
-    console.log(money.firstMoney);
+    var money = new Account(name, initialDeposit, deposit, withdrawal);
   $("#balance").text(money.firstMoney);
+    total = money.firstMoney;
 
+  //Transaction Submit
+    $("#transaction").submit(function(){
+      event.preventDefault();
+
+      var deposit = parseInt($("input#deposit").val());
+      var withdrawal = parseInt($("input#withdrawal").val());
+
+      var money = new Account(name, initialDeposit, deposit, withdrawal);
+      if (deposit > 0 && withdrawal > 0){
+        $("#balance").text(money.doubletrans());
+        total = money.doubletrans();
+      } else if (deposit > 0) {
+        $("#balance").text(money.addDeposit());
+        total = money.addDeposit();
+      } else {
+        $("#balance").text(money.subWithdra());
+        total = money.subWithdra();
+      }
+    });
+
+  //Transaction Submit
   });
+
  });
